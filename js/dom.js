a5a982cb69a8dac096fe27eedb5a4d05d1ab7a64
Ruslan_gg <107236487+Navielon@users.noreply.github.com>
2022-07-13 14:22:25 +0300

    upd

--- js/dom.js
+++ js/dom.js
@@ -22,25 +22,42 @@ let score = 20;
 console.log(score);
 
 document.querySelector('.check').addEventListener('click', function () {
-  const guessNumber = Number(document.querySelector('.number-input').value);
+const guessNumber = Number(document.querySelector('.number-input').value);
 //   console.log(typeof guessNumber, guessNumber);
 
-  if (!guessNumber) {
+if (!guessNumber) {
     // (!guessNumber = 0), Но если написать 0, то код не будет выполнятся, т.к. 0 = false. Поэтому используем подобную конвертацию
-    document.querySelector('.guess-message').textContent = 'Введите число!';
-  } else if (guessNumber === secretNumber) {
-    document.querySelector('.guess-message').textContent = 'Правильно!';
-    document.querySelector('.highscore').textContent = score;
-  } else if (guessNumber > secretNumber) {
-    document.querySelector('.guess-message').textContent =
+   document.querySelector('.guess-message').textContent = 'Введите число!';
+} 
+else if (guessNumber === secretNumber) {
+   document.querySelector('.guess-message').textContent = 'Правильно!';
+   document.querySelector('.highscore').textContent = score;
+} 
+else if (guessNumber > secretNumber) {
+      if (score > 1) {
+      document.querySelector('.guess-message').textContent =
       'Число слишком большое!';
       score--;
       document.querySelector('.score').textContent = score;
-  } else if (guessNumber < secretNumber) {
-    document.querySelector('.guess-message').textContent =
+   }
+   else {
+      document.querySelector('.guess-message').textContent =
+         'Game Over!';
+      document.querySelector('.score').textContent = 0;
+   }
+} 
+else if (guessNumber < secretNumber) {
+   if (score > 1) {
+      document.querySelector('.guess-message').textContent =
       'Число слишком маленькое!';
       score--;
       document.querySelector('.score').textContent = score;
-  }
-  
+   }
+   else {
+      document.querySelector('.guess-message').textContent =
+         'Game Over!';
+      document.querySelector('.score').textContent = 0;
+   }
+}
+
 });
